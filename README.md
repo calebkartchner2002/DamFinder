99335b87f693590b23b7e88040213fc2c6a85899
Thomas Stratford <22tstratford1@gmail.com>
2023-03-24 22:58:28 -0600

    something

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Authors: Caleb, Sam, Thomas
 
 # DamFinder
 Find best places to small dams
+
+Links:
+https://developers.arcgis.com/python/api-reference/arcgis.raster.functions.gbl.html?highlight=flow_direction
--- StreamData.py
+++ StreamData.py
@@ -6,55 +6,7 @@ class StreamData:
         self.vel = vel
         self.height = height
     def __str__(self):
-        return f"{self.name}: Latitude: {self.lat}, Longitude: {self.long}, Velocity: {self.vel}, Height: {self.height}"
-    def compareTo(self, other):
-        return self-other
-    
-def quicksort(arr):
-    quicksortRec(arr, 0, len(arr)-1)
-
-def quicksortRec(arr, left, right):
-
-    if left >= right:
-        return
-    pivot = int((right+left)/2)
-    updatedPivot = partition(arr, left, right, pivot)
-    quicksortRec(arr, left, updatedPivot-1)
-    quicksortRec(arr, updatedPivot+1, right)
-
-def partition(arr, beg, end, pivotPosition):
-    pivotElem = arr[pivotPosition]
-
-    swap(arr, pivotPosition, end)
-    right = end
-    left = beg
-    while(left<right):
-        while(arr[left] <= arr[pivotElem] and left<end):
-            left = left+1
-
-        while(arr[right] >= arr[pivotElem] and right>beg):
-            right = right-1
-        if left < right:
-            swap(arr, left, right)
-    swap(arr, left, end)
-    return left
-def swap(arr, first, second):
-    temp = arr[first]
-    arr[first] = arr[second]
-    arr[second] = temp
-
-
-
-
-test = [0]*10
-for i in range(10):
-    test[i] = 10-i
+        return f"{self.name}: Latitude: {self.lat}, Longitude: {self.long}, Velocity: {self.vel}, Height: {self.height}"   
 
 stream1 = StreamData("Here", 10, 10, 10, 10)
-stream2 = StreamData("Not Here", 20, 10, 5, 15)
-streamArr = [stream1, stream2]
-print(stream1.height)
 print(stream1)
-quicksort(test)
-for elem in test:
-    print(elem)
