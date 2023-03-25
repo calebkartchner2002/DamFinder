b87f84bfaea5a5dafa8ba8d9184bf2d5fd965cb5
Thomas Stratford <22tstratford1@gmail.com>
2023-03-24 19:21:54 -0600

    stream data

---
+++ StreamData.py
@@ -0,0 +1,13 @@
+class StreamData:
+    def __init__(self, name, lat, long, vel, height):
+        self.name = name
+        self.lat = lat
+        self.long = long
+        self.vel = vel
+        self.height = height
+    def __str__(self):
+        return f"{self.name}: Latitude: {self.lat}, Longitude: {self.long}, Velocity: {self.vel}, Height: {self.height}"
+
+
+stream1 = StreamData("Here", 10, 10, 10, 10)
+print(stream1)
