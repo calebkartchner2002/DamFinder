bbbe3a9480dabf26591c5a33171c45ee04471ff1
Thomas Stratford <22tstratford1@gmail.com>
2023-03-24 23:27:29 -0600

    compare

--- StreamDataTest.py
+++ StreamDataTest.py
@@ -1,4 +1,8 @@
 from StreamData import StreamData
 
 stream1 = StreamData("Here", 10, 10, 10, 10)
+stream2 = StreamData("L", 10, 10, 10, 20)
+streams = []
+streams.append(stream1)
+streams.append(stream2)
 print(stream1)
